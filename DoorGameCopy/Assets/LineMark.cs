319af8d0f936d59233dec17b2b01c7911efa3452
daguazi <daguazi@126.com>
2019-05-31 21:33:07 +0800

    no message

---
+++ DoorGameCopy/Assets/LineMark.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineMark : MonoBehaviour
+{
+    private GameObject clone;
+    private LineRenderer line;
+    private int i;
+    public GameObject obs;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clone = (GameObject)Instantiate(obs, obs.transform.position, transform.rotation);//克隆一个带有LineRender的物体   
+            line = GetComponent<LineRenderer>();//获得该物体上的LineRender组件  
+            line.SetColors(Color.blue, Color.red);//设置颜色  
+            line.SetWidth(0.2f, 0.1f);//设置宽度  
+            i = 0;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            i++;
+            line.SetVertexCount(i);//设置顶点数  
+            line.SetPosition(i - 1, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15)));//设置顶点位置   
+        }
+    }
+}
---
+++ DoorGameCopy/Assets/Scripts/AdjustScreen.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ((float)(Screen.width / (float)Screen.height) >= ((float)768 / (float)1024)) //当设备比较宽的时候
+        {
+            float size = 768 / 2 / ((float)(Screen.width / (float)Screen.height));
+            //if (size<=800) {
+            //    size = 800;
+            //}
+            transform.GetComponent<Camera>().orthographicSize = 512;
+        }
+        else
+        { //当设备比较窄的时候
+            float size = 768 / 2 / ((float)(Screen.width / (float)Screen.height));
+
+            transform.GetComponent<Camera>().orthographicSize = size;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
