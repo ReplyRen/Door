bc56cac2a8d6f2af5697b86a31ac4ef3437abc97
unknown <RYPHY0420@Gmail.com>
2019-06-03 14:11:25 +0800

    完成了传送门的传送

--- DoorGameCopy/Assets/Scripts/LineMark.cs
+++ DoorGameCopy/Assets/Scripts/LineMark.cs
@@ -18,7 +18,9 @@ public class LineMark : MonoBehaviour
     private float characterWidth ;
     private float timer = 0f;
     private GameObject player;
-    private List<Vector3> doorList = new List<Vector3>();
+    private List<Vector3> portalList = new List<Vector3>();
+    private List<Vector3> bHList = new List<Vector3>();
+    public int drawCount = 2;
 
     private enum vec3 { top,bottom,left,right,center}
     // Use this for initialization
@@ -27,101 +29,100 @@ public class LineMark : MonoBehaviour
         player = GameObject.FindWithTag("Player");
         characterWidth = player.GetComponent<BoxCollider2D>().size.x;
         characterHeight = player.GetComponent<BoxCollider2D>().size.y;
-        doorList.Clear();
+        portalList.Clear();
+        bHList.Clear();
     }
 
     void FixedUpdate()
     {
         timer += Time.deltaTime;
-        if (Input.GetMouseButtonDown(0))
+        if ((portalList.Count+bHList.Count) <= drawCount)
         {
-            clone = (GameObject)Instantiate(obs, obs.transform.position, transform.rotation);//克隆一个带有LineRender的物体   
-            line = clone.GetComponent<LineRenderer>();//获得该物体上的LineRender组件  
-            line.SetColors(Color.blue, Color.red);//设置颜色  
-            line.SetWidth(lineWidth, lineWidth);//设置宽度  
-            i = 0;
-            posList.Clear();
-            isPortalOpen = false;
-            isBlackHoleOpen = false;
-            timer = 0f;
-        }
-        if (Input.GetMouseButton(0) && isPortalOpen == false)
-        {
-            timer = 0f;
-            i++;
-            Vector3 pos = new Vector3();
-            line.positionCount=i;//设置顶点数  
-            pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15));
-            line.SetPosition(i - 1, pos);//设置顶点位置
-            if (posList.Count > 10)
+            if (Input.GetMouseButtonDown(0))
             {
-                for (int n = 0; n < posList.Count - 10; n++)
+                clone = (GameObject)Instantiate(obs, obs.transform.position, transform.rotation);//克隆一个带有LineRender的物体   
+                line = clone.GetComponent<LineRenderer>();//获得该物体上的LineRender组件  
+                line.SetColors(Color.blue, Color.red);//设置颜色  
+                line.SetWidth(lineWidth, lineWidth);//设置宽度  
+                i = 0;
+                posList.Clear();
+                isPortalOpen = false;
+                isBlackHoleOpen = false;
+                timer = 0f;
+            }
+            if (Input.GetMouseButton(0) && isPortalOpen == false)
+            {
+                timer = 0f;
+                i++;
+                Vector3 pos = new Vector3();
+                line.positionCount = i;//设置顶点数  
+                pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15));
+                line.SetPosition(i - 1, pos);//设置顶点位置
+                if (posList.Count > 10)
                 {
-                    if (IsClose(pos, posList[n]))
+                    for (int n = 0; n < posList.Count - 10; n++)
                     {
-                        isPortalOpen = true;
+                        if (IsClose(pos, posList[n]))
+                        {
+                            isPortalOpen = true;
+                        }
                     }
                 }
-            }
-            if (posList.Count == 0)
-                posList.Add(pos);
-            else if (pos != posList[posList.Count - 1])
-            {
-                posList.Add(pos);
-            }
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            IsBHOpen(posList);
-            List<Vector3> changeList = new List<Vector3>();
-            changeList = posList;
-            Vector3 topPoint = new Vector3();
-            Vector3 bottomPoint = new Vector3();
-            Vector3 leftPoint = new Vector3();
-            Vector3 rightPoint = new Vector3();
-            Vector3 centerPoint = new Vector3();
-            topPoint = posList[posList.Count - 1];
-            bottomPoint = GetPoint(changeList, vec3.bottom);
-            rightPoint = GetPoint(changeList, vec3.right);
-            leftPoint = GetPoint(changeList, vec3.left);
-            centerPoint = GetPoint(changeList, vec3.center);
-
-            if (isPortalOpen)
-            {
-                if ((topPoint.y - bottomPoint.y) < characterHeight || (rightPoint.x - leftPoint.x) < characterWidth)
+                if (posList.Count == 0)
+                    posList.Add(pos);
+                else if (pos != posList[posList.Count - 1])
                 {
-                    Debug.Log("太小了");
-                    Destroy(clone);
-                    clone.GetComponent<LineMark>().enabled = false;
+                    posList.Add(pos);
+                }
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                IsBHOpen(posList);
+                List<Vector3> changeList = new List<Vector3>();
+                changeList = posList;
+                Vector3 topPoint = new Vector3();
+                Vector3 bottomPoint = new Vector3();
+                Vector3 leftPoint = new Vector3();
+                Vector3 rightPoint = new Vector3();
+                Vector3 centerPoint = new Vector3();
+                topPoint = posList[posList.Count - 1];
+                bottomPoint = GetPoint(changeList, vec3.bottom);
+                rightPoint = GetPoint(changeList, vec3.right);
+                leftPoint = GetPoint(changeList, vec3.left);
+                centerPoint = GetPoint(changeList, vec3.center);
 
+                if (isPortalOpen)
+                {
+                    if ((topPoint.y - bottomPoint.y) < characterHeight || (rightPoint.x - leftPoint.x) < characterWidth)
+                    {
+                        Debug.Log("太小了");
+                        Destroy(clone);
+
+                    }
+                    else
+                    {
+                        Debug.Log("传送门");
+                        portalList.Add(centerPoint);
+                    }
+
+                }
+                else if (isBlackHoleOpen)
+                {
+                    Debug.Log("黑洞门");
+                    bHList.Add(centerPoint);
                 }
                 else
                 {
-                    Debug.Log("传送门");
+                    Destroy(clone);
 
-                    clone.GetComponent<LineMark>().enabled = false;
                 }
 
+                if (!isPortalOpen && !isBlackHoleOpen)
+                    Destroy(clone);
             }
-            else if (isBlackHoleOpen)
-            {
-                Debug.Log("黑洞门");
-                clone.GetComponent<LineMark>().enabled = false;
-            }
-            else
-            {
-                Destroy(clone);
-                clone.GetComponent<LineMark>().enabled = false;
-
-            }
-
-            if (!isPortalOpen && !isBlackHoleOpen)
-                Destroy(clone);
         }
-        //else if (timer > 2f)
-        //{
-        //    Destroy(clone);
-        //}
+        if (portalList.Count == 2)
+            Portal(portalList[0], portalList[1]);
         
     }
     private bool IsClose(Vector3 v1, Vector3 v2)
@@ -236,4 +237,11 @@ public class LineMark : MonoBehaviour
         
     }
 
+    private void Portal(Vector3 from,Vector3 to)
+    {
+        if (IsClose(player.transform.position,from))
+        {
+            player.transform.position = to;
+        }
+    }
 }
