203a3a332672677d823a6c7338e23e93bbd06b69
zjdx1998 <zjdx1998@gmail.com>
2019-07-18 01:52:41 +0800

    test line move

---
+++ DoorGameCopy/Assets/Scripts/ParticleStudy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleStudy : MonoBehaviour
+{
+    private ParticleSystem m_System;
+    ParticleSystem.Particle[] m_Paritcles;
+    public float m_Drift = 0.01f;
+
+    private void Awake()
+    {
+        if (m_System == null) m_System = GetComponent<ParticleSystem>();
+        if (m_Paritcles == null || m_Paritcles.Length < m_System.main.maxParticles)
+            m_Paritcles = new ParticleSystem.Particle[m_System.main.maxParticles];
+        var m_Main = m_System.main;
+        m_Main.simulationSpace = ParticleSystemSimulationSpace.World;
+    }
+
+    private void LateUpdate()
+    {
+        int numParticlesAlive = m_System.GetParticles(m_Paritcles);
+        ParticleSystem.Particle single = new ParticleSystem.Particle();
+        for (int i = 0; i < numParticlesAlive; i++)
+        {
+            Debug.Log(m_Paritcles[i].position);
+            //m_Paritcles[i].velocity += Vector3.up * m_Drift;
+        }
+        m_System.SetParticles(m_Paritcles, numParticlesAlive);
+        //Debug.Log(numParticlesAlive);
+    }
+}
---
+++ DoorGameCopy/Assets/lineMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineMove : MonoBehaviour
+{
+    public GameObject obs;
+    private GameObject clone;
+    private LineRenderer line;
+    public float lineWidth = 0.05f;
+    float m_Speed = 0.05f;
+    private int i;
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            clone = (GameObject)Instantiate(obs, obs.transform.position, transform.rotation);//克隆一个带有LineRender的物体   
+            line = clone.GetComponent<LineRenderer>();//获得该物体上的LineRender组件  
+            line.startColor = Color.black;//设置颜色  
+            line.startWidth = lineWidth;//设置宽度  
+            i = 0;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            if (clone != null)
+            {
+                i++;
+                Vector3 pos = new Vector3();
+                line.positionCount = i;//设置顶点数  
+                pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 15));
+                line.SetPosition(i - 1, pos);//设置顶点位置
+            }
+        }
+    }
+    private void LateUpdate()
+    {
+        if (clone == null) return;
+        int cnt = line.positionCount;
+        for(int i = 0; i < cnt; i++)
+        {
+            line.SetPosition(i, line.GetPosition(i) +  Vector3.left * m_Speed);
+        }
+    }
+}
